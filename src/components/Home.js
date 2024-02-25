7dd9106cc7b6ad094ccea4574815da238c1403b1
Makarem12 <makaremabososain@gmail.com>
2024-02-25 23:13:31 +0300

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import MovieList from "./MovieList";
 import ModalMovie from "./ModalMovie";
 import './Home.css';
 
 function Home() {
     const [moviesArr, setMoviesArr] = useState([]);
-    const [selectedMovie, setSelectedMovie] = useState(null); // Add this line to declare selectedMovie state
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
     const sendReq = async () => {
         const serverURL = `http://localhost:3001/trending`;
@@ -22,50 +19,11 @@ function Home() {
         sendReq();
     }, []);
 
-    const handleOpenModal = (movie) => {
-        setSelectedMovie(movie);
-    };
-
-    const handleCloseModal = () => {
-        setSelectedMovie(null);
-    };
-
-    const addToFavorites = (movie, comment) => {
-        
-        console.log("Adding to favorites:", movie, "with comment:", comment);
-    };
 
     return (
         <>
-            <Row xs={1} md={4} className="g-4">
-                {moviesArr.map((item) => {
-                    return (
-                        <Col key={item.id}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" alt="Movie image" src={item.path} />
-                                <Card.Body>
-                                    <Card.Title>ID: {item.id}</Card.Title>
-                                    <Card.Title>Name:{item.title}</Card.Title>
-                                    <Card.Title>Date: {item.date}</Card.Title>
-                                    <Card.Title>overview:</Card.Title>
-                                    <Card.Text>
-                                        {item.overview}
-                                    </Card.Text>
-                                    <Button variant="primary" onClick={() => handleOpenModal(item)}>Add to Fav</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    );
-                })}
-            </Row>
-            {selectedMovie && (
-                <ModalMovie
-                    movie={selectedMovie}
-                    addToFavorites={addToFavorites}
-                />
-            )}
-        </>
-    );
+           <MovieList moviesArr={moviesArr}/> </>) 
+      
 }
 
 export default Home;
